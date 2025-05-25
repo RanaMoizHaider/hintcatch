854e2a662f913ca72c6aef87f0e9aafc3b2a0cc9
Moiz Haider <ranamoizhaider@icloud.com>
2025-05-26 00:04:26 +0500

    add platform

---
+++ app/Models/Platform.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Platform extends Model
+{
+    /** @use HasFactory<\Database\Factories\PlatformFactory> */
+    use HasFactory;
+
+    protected $guarded = [];
+
+    protected $casts = [
+        'features' => 'array',
+        'best_practices' => 'array',
+    ];
+}
