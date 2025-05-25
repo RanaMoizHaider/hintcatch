41da06994908f646aa05193f24bfe5c554c6b319
Moiz Haider <ranamoizhaider@icloud.com>
2025-05-26 00:02:31 +0500

    add category

---
+++ database/factories/CategoryFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Category>
+ */
+class CategoryFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->word,
+            'slug' => $this->faker->slug,
+            'description' => $this->faker->sentence,
+            'image' => $this->faker->imageUrl,
+            'color' => $this->faker->hexColor,
+            'icon' => $this->faker->imageUrl,
+            'parent_id' => null,
+        ];
+    }
+}
