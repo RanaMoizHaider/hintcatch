6c3a1677a81a84a3e84ad7fa4e5854791a57ef07
Moiz Haider <ranamoizhaider@icloud.com>
2025-05-26 17:45:34 +0500

    update category

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Category extends Model
 {
@@ -12,13 +14,23 @@ class Category extends Model
 
     protected $guarded = [];
 
-    public function parent()
+    public function parent(): BelongsTo
     {
         return $this->belongsTo(Category::class, 'parent_id');
     }
 
-    public function children()
+    public function children(): HasMany
     {
         return $this->hasMany(Category::class, 'parent_id');
+    }
+
+    public function prompts(): HasMany
+    {
+        return $this->hasMany(Prompt::class);
+    }
+
+    public function countPrompts(): int
+    {
+        return $this->prompts()->count();
     }
 }
