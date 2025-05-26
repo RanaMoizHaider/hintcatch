499d0b8521b61e6ee98d859d2962c2a74debe019
Moiz Haider <ranamoizhaider@icloud.com>
2025-05-26 12:24:27 +0500

    add likes

---
+++ database/factories/LikeFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Prompt;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Like>
+ */
+class LikeFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'user_id' => User::factory(),
+            'likeable_id' => Prompt::factory(),
+            'likeable_type' => Prompt::class,
+        ];
+    }
+}
