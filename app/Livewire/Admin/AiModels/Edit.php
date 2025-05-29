872502fed30908da376e0368aaf75deb2a3ecdb5
Moiz Haider <ranamoizhaider@icloud.com>
2025-05-29 20:29:55 +0500

    adds livewire components

---
+++ app/Livewire/Admin/AiModels/Edit.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Admin\AiModels;
+
+use Livewire\Component;
+
+class Edit extends Component
+{
+    public function render()
+    {
+        return view('livewire.admin.ai-models.edit');
+    }
+}
---
+++ app/Livewire/Admin/Platform/Create.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Admin\Platform;
+
+use Livewire\Component;
+
+class Create extends Component
+{
+    public function render()
+    {
+        return view('livewire.admin.platform.create');
+    }
+}
